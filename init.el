905746a630cf00db7b27940277a08508fd535da8
Eric Haberstroh <gpg@erixpage.de>
2018-07-20 17:38:59 +0200

    Remove Custom code from init and stop the Customize madness

--- init.el
+++ init.el
@@ -76,17 +76,3 @@
 (makunbound 'eh/original-gc-cons-threshold)
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (magit nlinum-relative org-bullets htmlize zenburn-theme use-package spacemacs-theme diminish cycle-themes))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
